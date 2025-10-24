b290753a088ac35ca1dfa2f978d9a45aa01b48f5
Ana Miranda <amirandalo@unsa.edu.pe>
2025-10-24 18:11:23 -0500

    Subir ejercicio05.js corregido

--- Ejercicios/ejericicio05.js
+++ Ejercicios/ejericicio05.js
@@ -4,10 +4,15 @@ alert("El programa se ejecutará de manera indefinida "+
 
 //Crear un bucle para que sea iterativo
 while(true) {
-    let monto = parseDouble(prompt("Ingrese el monto:"));
+    let monto = parseFloat(prompt("Ingrese el monto:"));
 
-    if (monto == -1){
+    if (monto === -1){
         break;
+    }
+
+    if (isNaN(monto)) {
+        alert("Por favor, ingrese un número válido.");
+        continue;
     }
 
     let contador100 = 0;
@@ -17,16 +22,18 @@ while(true) {
 
     let residuo = monto;
 
-    while (residuo > 0){
-        contador100 = Math.trunc(monto/100);
-        residuo = monto % 100;
-        contador50 = Math.trunc(residuo/50);
-        residuo = residuo % 50;
-        contador20 = Math.trunc(residuo/20);
-        residuo = residuo % 20;
-        contador10 = Math.trunc(residuo/10);
-        residuo = monto % 10;
-    }
+    contador100 = Math.trunc(residuo/100);
+    residuo = monto % 100;
+    
+    contador50 = Math.trunc(residuo/50);
+    residuo = residuo % 50;
+
+    contador20 = Math.trunc(residuo/20);
+    residuo = residuo % 20;
+
+    contador10 = Math.trunc(residuo/10);
+    residuo = residuo % 10;
+    
 
     //Mostrar resultados 
     alert("Resultados \nBilletes de 100: "+contador100+
@@ -34,3 +41,6 @@ while(true) {
         "\nBilletes de 20: "+contador20+
         "\nBilletes de 10: "+contador10)
 }
+
+alert("Gracias :D");
+
